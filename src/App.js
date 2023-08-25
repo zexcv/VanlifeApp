3c121b857d374a0554624c4498259c0ddedd106a
zexcv <krusher13@o2.pl>
2023-08-25 13:03:58 +0200

    Basic style and gitignore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import About from './components/About';
+import Home from './components/Home';
+
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+  <div className='App'>
+    <BrowserRouter>
+        <header>
+          <Link to="/">#VANLIFE</Link>
+          <nav>
+            <Link to="/about">About</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+        </Routes>
+      </BrowserRouter>
+  </div>
   );
 }
 
