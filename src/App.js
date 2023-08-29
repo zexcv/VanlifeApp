192eaeaa842a053a83e69b3c55a485a4ac76ff74
zexcv <krusher13@o2.pl>
2023-08-30 00:29:40 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import About from './components/About';
 import Home from './components/Home';
+import Vans from './components/Vans';
 
+import './server'
 
 
 function App() {
@@ -13,11 +15,13 @@ function App() {
           <Link to="/">#VANLIFE</Link>
           <nav>
             <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
           </nav>
         </header>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />}/>
+          <Route path="/vans" element={<Vans />}/>
         </Routes>
       </BrowserRouter>
   </div>
